dd7a84006e41c9ff4b3406ad6a854ec8f006afda
ismayil-allahverdiyev <allahverdiyev.ismayl@mail.ru>
2023-03-28 03:17:50 +0300

    Category cover image upload

--- routes/categories.js
+++ routes/categories.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const mongoose = require("mongoose")
+
+const index = require("../index")
+
 const imageUploadGfs = require("../middlewares/imageGfsUpload")
 const Category = require("../models/category_model")
 
 const categoryRouter = express.Router()
 
-const imageUrl = "https://aisha-sales-app.herokuapp.com/api/images/"
+const imageUrl = "https://aisha-sales-app.herokuapp.com/api/categoryCover/"
 
 categoryRouter.get("/categories", async (req, res) =>{
     const categories = await Category.find();
@@ -30,6 +33,26 @@ categoryRouter.post("/newCategory", imageUploadGfs.single("image"), async (req, 
         }
     }else{
         res.send("The \"" + title + "\" category already exists")
+    }
+})
+
+categoryRouter.get("/api/categoryCover/:filename", (req, res) => {
+    try{
+        const parFilename = req.params.filename
+        console.log(parFilename)
+
+        index.gfs = new mongoose.mongo.GridFSBucket(mongoose.connection, {
+            bucketName: "category_covers"
+        })
+
+        const file = index.gfs.openDownloadStreamByName(parFilename);
+        file.on("error", function(err){
+            res.send("No image found with the given id!")
+        });
+        file.pipe(res)
+    }catch(e){
+        console.log(e)
+        res.send(e)
     }
 })
 
