88ef5bdcee21af047cdac4210f57d1c694e93db3
ismayil-allahverdiyev <allahverdiyev.ismayl@mail.ru>
2023-04-01 13:58:22 +0300

    req.body

--- routes/poster.js
+++ routes/poster.js
@@ -46,7 +46,7 @@ posterRouter.get("/api/getAllPosters", async (req, res)=>{
 posterRouter.post("/api/addPoster", posterImageUploadGfs.array("image"), async (req, res)=>{
     console.log("QUQU")
     const{userId, category, price, title} = req.body;
-    console.log(category);
+    console.log("req.body " + req.body);
 
     const categoryExists = await Category.findOne({"title": category})
 
