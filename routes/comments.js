ed1d6fd3eb7110272d3cfe54d4d136310b7a74a5
ismayil-allahverdiyev <allahverdiyev.ismayl@mail.ru>
2023-04-18 18:54:25 +0300

    Commenting basic form added

--- routes/comments.js
+++ routes/comments.js
@@ -1,21 +1,50 @@
 const mongoose = require("mongoose");
 const express = require("express");
+const User = require("../models/user");
+const bcryptjs = require("bcryptjs");
+const Comment = require("../models/comment_model");
 
 const commentsRouter = express.Router();
 
 commentsRouter.post("/comments/addComment", async (req, res) => {
-    const {userId, description} = req.body;
+    const {token, email, description} = req.body;
+
     console.log({
-        userId,
+        token,
         description
     });
+
+
+
+    const user = await User.findOne({email});
+
+    const compared = await bcryptjs.compare(token, user.password);
+
+    if(!compared){
+        return res.status(400).json({
+            msg: "Incorrect password!"
+        });
+    }
+
     try {
-        res.status(200).json({
-            userId,
-            description
-        })
+
+        const date = Date.now();
+
+        let comment = Comment({
+            description,
+            "userId" : user.userId,
+            "username" : "User Userov",
+            "date" : date.toString(),
+        });
+
+        return res.status(200).json({
+            token,
+            description,
+            user,
+            comment
+        });
     } catch (error) {
-        res.status(400).send(error);
+        return res.status(400).send(error);
     }
 })
 
