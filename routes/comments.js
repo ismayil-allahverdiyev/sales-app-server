64f373e232d642dd69d3c8c088a27572a952c152
ismayil-allahverdiyev <allahverdiyev.ismayl@mail.ru>
2023-04-21 17:33:46 +0300

    getCommentsById fix res.json

--- routes/comments.js
+++ routes/comments.js
@@ -53,7 +53,7 @@ commentsRouter.post("/comments/addComment", async (req, res) => {
 
 commentsRouter.get("/comments/getCommentsById", (req, res) => {
     const{posterId} = req.params;
-    res.send(posterId)
+    res.json(posterId)
 })
 
 module.exports = commentsRouter;
