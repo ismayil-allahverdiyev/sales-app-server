2bc1efe21c492a8d74d06b10ea68941dd3b81f4d
ismayil-allahverdiyev <allahverdiyev.ismayl@mail.ru>
2023-05-18 15:50:31 +0300

    fix to the logs

--- controllers/auth_controller.js
+++ controllers/auth_controller.js
@@ -3,9 +3,9 @@ const User = require("../models/user");
 
 const jwtVerifier = async (token) => {
     const verified = await jwt.verify(token, "passwordKey");
-    print("Verified id is " + verified.id)
+    console.log("Verified id is " + verified.id)
     const user = await User.findById(verified.id);
-    print("User id is " + user.id)
+    console.log("User id is " + user.id)
 
     console.log("Comparison is " + user)
 
