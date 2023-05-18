5ab126b1498337d8f626912ade58f8609582a571
ismayil-allahverdiyev <allahverdiyev.ismayl@mail.ru>
2023-05-18 16:09:13 +0300

    remove from basket added

--- routes/basket.js
+++ routes/basket.js
@@ -44,4 +44,42 @@ basketRouter.post("/basket/addToBasket", async (req, res) => {
     return res.json(updatedUser)
 })
 
+basketRouter.post("/basket/removeFromBasket", async (req, res) => {
+    const {token, posterId} = req.body
+
+    const user = await jwtVerifier(token)
+    console.log("USER is " + user)
+    if(!user){
+        return res.status(400).json({
+            msg: "User not found!",
+        })
+    }
+
+    const poster = await Poster.findById(posterId)
+
+    if(!poster){
+        return res.status(400).json({
+            msg: "Poster not found!",
+        })
+    }
+
+    const posterInfo = {
+        id: poster.id,
+        description: poster.title,
+        price: poster.price,
+    }
+    console.log("Basket includes " + user.basket);
+    console.log("Is in there " + user.basket.includes(posterInfo));
+    
+    const updatedUser = await user.update(
+        
+        {$pull: {
+            basket: posterInfo
+            },
+        },
+    )
+    console.log("Update is " + updatedUser)
+    return res.json(updatedUser)
+})
+
 module.exports = basketRouter;
