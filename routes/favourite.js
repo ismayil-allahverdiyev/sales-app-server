161f4221918fc208eb1f1447ce9493ddc2c9529b
ismayil-allahverdiyev <allahverdiyev.ismayl@mail.ru>
2023-05-28 12:11:27 +0300

    addToFavourites updated

--- routes/favourite.js
+++ routes/favourite.js
@@ -3,11 +3,50 @@ const mongoose = require("mongoose")
 const { validate } = require("../models/user")
 const { jwtVerifier } = require("../controllers/auth_controller")
 const User = require("../models/user")
+const Poster = require("../models/poster_model")
 
 const favouriteRouter = express.Router()
 
 favouriteRouter.post("/api/addToFavourites", async (req, res) => {
-    const{posterId, image, token} = req.body
+    const{posterId, token} = req.body
+    
+    const user = await jwtVerifier(token)
+
+    if(!user){
+        return res.status(400).json({
+            "msg": "User not found!",
+        })
+    }
+
+    const poster = await Poster.findById(posterId)
+
+    for(const favourite of user.favourites){
+        if(favourite["id"] == posterId){
+            return res.status(400).json({
+                msg: "Poster is already in favourites!",
+            })
+        }
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+        user.id,
+        {
+            $pull: {
+                favourites: {
+                    "id": posterId,
+                    "image": poster.image[0],
+                    "price": poster.price,
+                    "title": poster.title,
+                },
+            }
+        }
+    )
+
+    return res.json(updatedUser)
+})
+
+favouriteRouter.post("/api/removeFromFavourites", async (req, res) => {
+    const{posterId, image, price, description, token} = req.body
     
     const user = await jwtVerifier(token)
 
@@ -32,6 +71,8 @@ favouriteRouter.post("/api/addToFavourites", async (req, res) => {
                 favourites: {
                     "id": posterId,
                     "image": image,
+                    price,
+                    description,
                 },
             }
         }
