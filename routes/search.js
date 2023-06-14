412565c7afdaa4eca1b14e975885bcd6758ea160
ismayil-allahverdiyev <allahverdiyev.ismayl@mail.ru>
2023-06-14 13:24:19 +0300

    searchByCategoriesAndTitle added

--- routes/search.js
+++ routes/search.js
@@ -34,7 +34,7 @@ searchRouter.get("/api/search", async (req, res) => {
 searchRouter.get("/api/searchByCategories", async (req, res) => {
     try{
         const token = req.query.token;
-        const keywords = req.query.keywords;
+        const categories = req.query.categories;
 
         const user = await jwtVerifier(token)
         if(!user){
@@ -43,11 +43,35 @@ searchRouter.get("/api/searchByCategories", async (req, res) => {
             })
         }
 
-        var posters = await Poster.find({category: {$regex: keywords.join('|'), $options: "i"}})
+        var posters = await Poster.find({category: {$regex: categories.join('|'), $options: "i"}})
         
-        return res.status(200).json({
-            "posters" : posters,
+        return res.status(200).json(posters)
+    }catch(e){
+        return res.status(500).json({
+            error: e.message
         })
+    }
+})
+
+searchRouter.get("/api/searchByCategoriesAndTitle", async (req, res) => {
+    try{
+        const token = req.query.token;
+        const categories = req.query.categories;
+        const keyword = req.query.keyword;
+
+        const user = await jwtVerifier(token)
+        if(!user){
+            return res.status(404).json({
+                msg: "User not found!",
+            })
+        }
+
+        var posters = await Poster.find({
+            category: {$regex: categories.join('|'), $options: "i"},
+            title: {$regex: keyword, $options: "i"}
+        })
+        
+        return res.status(200).json(posters)
     }catch(e){
         return res.status(500).json({
             error: e.message
