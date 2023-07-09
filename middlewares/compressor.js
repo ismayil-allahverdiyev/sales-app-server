3a0412f39e0890e363293829b9225f7cf124dd3a
Ismayil Allahverdiyev <allahverdiyev.ismayl@mail.ru>
2023-07-09 11:00:14 +0300

    more logs added

--- middlewares/compressor.js
+++ middlewares/compressor.js
@@ -4,12 +4,14 @@ const compressImages = async (req, res, next) => {
   try {
     if (req.files) {
       for (const element of req.files) {
-        console.log("Compressor working")
+        console.log("Compressor working 1")
         const buffer = await sharp(element.buffer)
           .resize({ quality: 70 })
           .toBuffer();
-          
+        console.log("Compressor working 2")
         element.buffer = buffer;
+        console.log("Compressor working 3")
+
       }
     }
     
