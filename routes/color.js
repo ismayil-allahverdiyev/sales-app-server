b8a14d1e902d43b07937c65f2bc765b3bed0115b
Ismayil Allahverdiyev <allahverdiyev.ismayl@mail.ru>
2023-08-15 14:59:27 +0300

    adding only one type of color (still on work)

--- routes/color.js
+++ routes/color.js
@@ -4,9 +4,17 @@ const Color = require("../models/color_model")
 
 const colorRouter = express.Router()
 
-colorRouter.post("/api/color/addNewColor", async (req, res) => {
+colorRouter.post("/api/color/addNewColors", async (req, res) => {
     try {
-        const { colorName, hexCodes } = req.body
+        const { colorName, hexCodes } = req.body// add color as a list
+
+        let exsistingColor = await Color.findOne({ colorName })
+
+        if (exsistingColor && exsistingColor.hexCodes.contains()) {
+            return res.status(400).json({
+                "msg": "Color already found!",
+            })
+        }
 
         let color = Color({
             colorName,
@@ -17,7 +25,7 @@ colorRouter.post("/api/color/addNewColor", async (req, res) => {
 
         console.log("COLOR IS " + color)
 
-        res.status(400).json({
+        return res.status(200).json({
             color
         })
     } catch (error) {
@@ -33,7 +41,19 @@ colorRouter.get("/api/color/searchByName", async (req, res) => {
 
         let color = await Color.findOne({ colorName })
 
-        res.status(400).json(color)
+        res.status(200).json(color)
+    } catch (error) {
+        res.status(500).json({
+            error: e.message
+        })
+    }
+})
+
+colorRouter.get("/api/color/getAll", async (req, res) => {
+    try {
+        let colors = await Color.find()
+
+        res.status(200).json(colors)
     } catch (error) {
         res.status(500).json({
             error: e.message
