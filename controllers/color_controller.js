ac97e371683108a0f33a0c817da2afba81d255dd
Ismayil Allahverdiyev <allahverdiyev.ismayl@mail.ru>
2023-09-06 02:38:11 +0300

    fixing

--- controllers/color_controller.js
+++ controllers/color_controller.js
@@ -3,7 +3,7 @@ const Color = require("../models/color_model");
 const colorUploader = async (colors) => {
     try {
         for (const element of colors) {
-            console.log(element)
+            console.log(element["colorName"])
             let existingColor = await Color.findOne({ "colorName": element["colorName"] })
 
             if (!existingColor) {
@@ -12,16 +12,16 @@ const colorUploader = async (colors) => {
                     hexCodes: [element["hexCode"],],
                 })
                 color = await color.save()
-                            console.log("in uploader 1")
+                console.log("in uploader 1")
             } else if (!existingColor.hexCodes.includes(element["hexCode"])) {
                 existingColor.hexCodes.push(element["hexCode"])
                 existingColor = await existingColor.save()
-                                            console.log("in uploader 2")
+                console.log("in uploader 2")
             }
         }
         return true
     } catch (e) {
-                                    console.log("in uploader error" + e)
+        console.log("in uploader error" + e)
 
         return false
     }
