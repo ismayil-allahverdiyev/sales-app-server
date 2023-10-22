418a4f4c1831ca6d60eb0cd13544658a6c05e90a
Ismayil Allahverdiyev <allahverdiyev.ismayl@mail.ru>
2023-10-22 16:01:50 +0300

    getposters with isFavoured logs added

--- routes/poster.js
+++ routes/poster.js
@@ -46,7 +46,7 @@ posterRouter.get("/api/getAllPosters", async (req, res) => {
 
         for(const poster of posters){
             let isFavoured = userFavourites.includes(poster["_id"]);
-            console.log(userFavourites + " " + poster["_id"])
+            console.log(userFavourites + " " + poster["_id"] + " " + isFavoured)
 
             const displayPosterModel = new DisplayPosterModel(poster["category"], poster["title"], poster["coverImage"], isFavoured, poster["colorPalette"])
             result.push(displayPosterModel)
